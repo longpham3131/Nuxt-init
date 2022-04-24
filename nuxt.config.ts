1fdca6cbfe7ca6418135ddfd290dbebe838db1f3
Hoang Long Pham <hoanglongpham@deevotech.com>
2022-04-24 21:13:01 +0700

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  plugins: ["@/plugins/vue-gtag"],
-});
+export default defineNuxtConfig({});
--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -3,7 +3,7 @@ import VueGtag from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     config: {
-      id: "G-W7PBVMBCSH",
+      id: "G-EE57YGSJGV",
     },
   });
 });
