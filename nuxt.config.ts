132be2946eab5c8ac2a8f75bfbf45a0bd385a388
Hoang Long Pham <hoanglongpham@deevotech.com>
2022-04-24 22:43:03 +0700

    thua

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  buildModules: ["@nuxtjs/google-analytics"],
-  googleAnalytics: {
-    id: "G-W7PBVMBCSH",
-  },
-});
+export default defineNuxtConfig({});
