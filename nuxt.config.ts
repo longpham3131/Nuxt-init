0f8d2bc5147862a20738329a313ba32cff6f16ef
Hoang Long Pham <hoanglongpham@deevotech.com>
2022-04-24 22:15:07 +0700

    last hope

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  buildModules: ["@nuxtjs/google-analytics"],
+  googleAnalytics: {
+    id: "G-W7PBVMBCSH", // Use as fallback if no runtime config is provided
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: "G-W7PBVMBCSH",
+    },
+  },
+});
