6a6b9c343576be52271c46fee6b4331bbad4011d
Hoang Long Pham <hoanglongpham@deevotech.com>
2022-04-24 21:39:22 +0700

    alalal

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,9 +1,9 @@
-import VueGtag from "vue-gtag";
+import VueGtag from "vue-gtag-next";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
-    config: {
-      id: "G-EE57YGSJGV",
+    property: {
+      id: "G-W7PBVMBCSH",
     },
   });
 });
