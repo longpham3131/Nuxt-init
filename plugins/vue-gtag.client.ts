0fffcd0105c30bc2369420a5c6ce0ec262350637
Hoang Long Pham <hoanglongpham@deevotech.com>
2022-04-24 22:57:43 +0700

    no hope

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -3,7 +3,7 @@ import VueGtag from "vue-gtag-next";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
-      id: "UA-226689837-1",
+      id: "G-HBK340SDH2",
     },
     useDebugger: true,
   });
